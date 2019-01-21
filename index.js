3323e961895880026e665e1766305711c0d7040d
Toktam  Rezaei <toktamrezaei23@gmail.com>
2019-01-21 17:13:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 /* Enter the code to remove the main node element under this comment */
 
+const main = document.querySelector('main')
+main.remove()
+
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+const newHeader = document.createElement('h1')
+newHeader.innerText = "Toktam is a cool person"
+document.body.appendChild(newHeader)
+
+
